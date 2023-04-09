9636ecaa43de91e14ed7dbc228fad8b7ab4f16da
Ilya <zobolew.i@gmail.com>
2023-04-09 16:53:42 +0300

    Camera fix

--- Assets/OLD/Assets/Scripts/CameraScript.cs
+++ Assets/OLD/Assets/Scripts/CameraScript.cs
@@ -1,26 +1,65 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [Space]
-    [SerializeField] private Transform _player1;
-    [SerializeField] private Transform _player2;
+    [SerializeField] private List<Player> _players;
 
-    private Vector3 _cameraTargetPosition;
-    
-    void Update()
+    private void Awake()
     {
-        _cameraTargetPosition = new Vector2((_player1.position.x + _player2.position.x) / 2, transform.position.y);
+        Player.PlayerSpawned += OnPlayerSpawned;
+        Player.PlayerDestroyed += OnPlayerDestroyed;
+    }
+
+    private void OnPlayerSpawned(Player player) => _players.Add(player);
+    private void OnPlayerDestroyed(Player player) => _players.Remove(player);
+
+
+    private void Update()
+    {
+        if (_players.Count == 0)
+            return;
+
+        var cameraTargetPosition = new Vector2(CalculatePlayersMedianPoint().x, transform.position.y);
         
-        transform.position = Vector2.Lerp(transform.position, _cameraTargetPosition, 0.015f);
+        transform.position = Vector2.Lerp(transform.position, cameraTargetPosition, 0.015f);
 
         float minCameraSize = 11.3f;
         float maxCameraSize = 15f;
 
-        var distanceBtwPlayers = Vector2.Distance(_player1.position, _player2.position);
+        var distanceBtwPlayers = FindMaxDistanceBtwPlayer();
         var targetCameraSize = Mathf.Lerp(minCameraSize, maxCameraSize, Mathf.InverseLerp(15, 30, distanceBtwPlayers));
 
         _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, targetCameraSize, 0.015f);
+    }
+
+    private Vector2 CalculatePlayersMedianPoint()
+    {
+        var positionsSum = Vector3.zero;
+
+        foreach (var player in _players)
+            positionsSum += player.transform.position;
+
+        return positionsSum / _players.Count;
+    }
+
+    private float FindMaxDistanceBtwPlayer()
+    {
+        var maxDistance = 0f;
+
+        foreach (var player1 in _players)
+        {
+            foreach (var player2 in _players)
+            {
+                var distanceBtwPlayer = Vector2.Distance(player1.transform.position, player2.transform.position);
+
+                if (distanceBtwPlayer > maxDistance)
+                    maxDistance = distanceBtwPlayer;
+            }
+        }
+
+        return maxDistance;
     }
 }
